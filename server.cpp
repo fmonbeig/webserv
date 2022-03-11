5a6bb1ff9dc1bca1aba6628325c7d550464e27b1
Florian MONBEIG <fmonbeig@e2r3p19.clusters.42paris.fr>
2022-03-11 18:38:32 +0100

    hop

--- server.cpp
+++ server.cpp
@@ -6,7 +6,7 @@
 /*   By: fmonbeig <fmonbeig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 17:50:15 by fmonbeig          #+#    #+#             */
-/*   Updated: 2022/03/10 17:44:13 by fmonbeig         ###   ########.fr       */
+/*   Updated: 2022/03/11 18:38:13 by fmonbeig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,27 +23,87 @@ static Socket*	createSocket(int port)
 	return (new_sock);
 }
 
-//     int poll(struct pollfd fds[], nfds_t nfds, int timeout);
-// fds is our array of information (which sockets to monitor for what), nfds is the count of elements in the array, and timeout is a timeout in milliseconds. It returns the number of elements in the array that have had an event occur.
-
-// Let’s have a look at that struct:
-
-//     struct pollfd {
-//         int fd;         // the socket descriptor
-//         short events;   // bitmap of events we're interested in
-//         short revents;  // when poll() returns, bitmap of events that occurred
-//     };
-
 static struct pollfd*	createPollfd(std::vector<Socket*> & socket) //create structure for poll() to listen at every socket
 {
 	struct pollfd *poll = new pollfd[socket.size()];
 	for(int i = 0; i < socket.size(); i++)
 	{
-		poll->fd = socket[i]->getSocketFd();
-		poll->events = POLLIN;
+		poll[i].fd = socket[i]->getSocketFd();
+		poll[i].events = POLLIN;
 	}
 	return (poll);
 }
+
+void	recv_and_send(int link)
+{
+	// char	buff[90000];
+	// int		ret;
+	// std::string hello= "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+
+	// memset((void*)buff, 0, 90000);
+	// if ((ret = recv(link, buff, 90000, 0)) < 0)
+	// {
+	// 	std::perror("Recv failed:");
+	// 	return ;
+	// }
+	// std::cout << *buff << std::endl;
+	// if ((ret = send(link,  hello.c_str() , hello.size(), 0)) < 0) // send() could not send all the data at once sometimes
+	// {
+	// 	std::perror("Send failed:");
+	// 	return ;
+	// }
+	// std::cout << "\n+++++++ Hello message sent ++++++++\n" << std::endl;
+
+		int valread;
+		char buffer[30000] = {0};
+		std::string hello= "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+		valread = read( link , buffer, 30000);
+		printf("%s\n",buffer );
+
+		write(link , hello.c_str() , hello.size());
+		printf("------------------Hello message sent-------------------");
+
+
+}
+
+static void	port_listening(std::vector<Socket*> & socket, struct pollfd* poll_fd)
+{
+	int	poll_ret;
+	int	link;
+
+	while (1)
+	{
+		std::cout << "\n+++++++ Waiting for new connection ++++++++\n" << std::endl;
+		std::cout << "poll fd " << poll_fd[0].fd << std::endl;
+		std::cout << "socket fd " << socket[0]->getSocketFd() << std::endl;
+		std::cout << "socket size " << socket.size() << std::endl;
+		poll_ret = poll(poll_fd, socket.size(), 2500); // with negative number poll never time out
+		std::cout << "result of poll_ret : " << poll_ret << std::endl;
+
+		if (poll_ret == 0)
+		{
+			std::cout << " Server Time out" << std::endl; // related to the timeout parameter of poll
+		}
+		else
+		{
+			for(int i = 0; poll_ret > 0; i++)
+			{
+				std::cout << "TEST " << std::endl;
+				if (poll_fd[i].revents & POLLIN)
+				{
+					std::cout << "BONJOUR " << std::endl;
+					poll_ret--;
+					if ((link = accept(socket[i]->getSocketFd(), (struct sockaddr *)&socket[i]->_address, (socklen_t*)&socket[i]->_addrlen))<0)
+						std::perror("Accept failed:");
+					recv_and_send(link);
+					close(link);
+				}
+			}
+		}
+	}
+}
+
+
 
 int main()
 {
@@ -51,7 +111,7 @@ int main()
 	std::vector<int>	allPort;
 	allPort.push_back(8080);
 	allPort.push_back(8001);
-	allPort.push_back(8002);
+	allPort.push_back(9034);
 
 	//Create a containers of Socket pointer. We initialize the bind and the listening for every Socket
 	std::vector<Socket*>	socket;
@@ -60,70 +120,7 @@ int main()
 
 	//Create the struct of pollfd for function poll
 	struct pollfd*	poll_fd;
-	int				poll_ret;
-	int				pollin_ok;
-	int				j = 0;
-	poll = createPollfd(socket);
 
-	//Create the loop for Listening all the opening port with poll
-	while (1)
-	{
-		poll_ret = poll(poll_fd, socket.size(), -1); // with negative number poll never time out
-
-		if (poll_ret == 0)
-			std::cout << " Nothing happened // Time out" << std::endl; // probably related to the timeout parameter of poll
-		else
-		{
-			for(int i = 0; j == poll_ret; i++)
-			{
-				pollin_ok = poll_fd[i].revents & POLLIN;
-				if (pollin_ok)
-				{
-					//READ with Accept we can do another function
-					j++;
-				}
-			}
-		}
-	// 	    if (num_events == 0) {
-    //     printf("Poll timed out!\n");
-    // } else {
-    //     int pollin_happened = pfds[0].revents & POLLIN; //TODO understand the BITWISE OPERATION
-
-    //     if (pollin_happened) {
-    //         printf("File descriptor %d is ready to read\n", pfds[0].fd);
-    //     } else {
-    //         printf("Unexpected event occurred: %d\n", pfds[0].revents);
-    //     }
-    // }
-	}
-
-
-
-
-
-
-	int new_socket;
-	int valread;
-
-	std::string hello= "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-
-	// regarder du coté de select pour capter les infos de plusieurs socket
-	while(1)
-	{
-		printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-		if ((new_socket = accept(socket[0]->getSocketFd(), (struct sockaddr *)&socket[0]->_address, (socklen_t*)&socket[0]->_addrlen))<0)
-		{
-			perror("In accept");
-			exit(EXIT_FAILURE);
-		}
-
-		char buffer[30000] = {0};
-		valread = read( new_socket , buffer, 30000);
-		printf("%s\n",buffer );
-		write(new_socket , hello.c_str() , hello.size());
-		printf("------------------Hello message sent-------------------");
-		close(new_socket);
-	}
-	return 0;
+	poll_fd = createPollfd(socket);
+	port_listening(socket, poll_fd);
 }
-
